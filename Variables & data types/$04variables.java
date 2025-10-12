89fb3c054968ebd69bf9937a7037291c678f4d1a
Amteshwar Singh <amteshwars8@gmail.com>
2025-10-12 17:42:01 +0530

    Variables

---
+++ Variables & data types/$04variables.java
@@ -0,0 +1,20 @@
+
+//2*(A+B) Here a and b are variables and 2 is literal
+// name of variables,class,function are called identifiers
+public class $04variables {
+
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "KAHLON";
+        System.out.println(name);
+
+        a = 50;
+        System.out.println(a);
+        a=b;
+        System.out.println(a);
+
+    }
+}
