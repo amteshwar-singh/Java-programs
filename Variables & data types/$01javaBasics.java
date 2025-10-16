a6eee1accb90059875fa955f7bc3280b7665e141
Amteshwar Singh <amteshwars8@gmail.com>
2025-10-16 23:32:36 +0530

    Delete Variables & data types directory

--- Variables & data types/$01javaBasics.java
+++
@@ -1,7 +0,0 @@
-// boiler plate code
-
-public class $01javaBasics {
-
-    public static void main(String args[]) {
-    }
-}
--- Variables & data types/$07Comments.java
+++
@@ -1,8 +0,0 @@
-public class $07Comments {
-public static void main(String args[]) {
-    // You can write anything in comments. compiler ignores it
-    /*
-    hi there
-    */
-}
-}
