86c59b16ecaf06b0f0a77f91a8b2415a41690b44
Amteshwar Singh <amteshwars8@gmail.com>
2025-10-17 23:17:25 +0530

    same

--- 01Variables & data types/$13TypeCasting.java
+++ 01Variables & data types/$13TypeCasting.java
@@ -1,11 +1,10 @@
 // jabardasti convert krona
 // also known as narrowing or explicit conversion
-import java.util.*;
 
 public class $13TypeCasting {
 
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+        
         float a = 22.55f;
         //int b=a this is wrong
         int b=(int)a;
--- 01Variables & data types/$17TypePromotion3.java
+++ 01Variables & data types/$17TypePromotion3.java
@@ -1,8 +1,9 @@
-import java.util.*;
+
 public class $17TypePromotion3 {
-public static void main(String args[]) {
-    byte b=5;
-    byte a=(byte)(b*2);
-    System.out.println(a);
-}
+
+    public static void main(String args[]) {
+        byte b = 5;
+        byte a = (byte) (b * 2);
+        System.out.println(a);
+    }
 }
