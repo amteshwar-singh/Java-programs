b84893d6aa725dcc316344ae66f6c1007e422295
Amteshwar Singh <amteshwars8@gmail.com>
2026-09-26 18:43:55 +0530

    modified kadan method

--- 08arrays/$11maxsubArray.java
+++ 08arrays/$11maxsubArray.java
@@ -18,12 +18,28 @@
             }
             System.out.println("maxSum= " + maxSum);
         }
+    public static void maxSubArray(int numberss[]) {
+        int currentSum = numberss[0];
+        int maxSum = numberss[0];
+
+        for (int i = 1; i < numberss.length; i++) {
+            // either extend the previous subarray, or start fresh at numbers[i]
+            currentSum = Math.max(numberss[i], currentSum + numberss[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        System.out.println("maxSum= " + maxSum);
+    }
 
     
 
     public static void main(String args[]) {
-        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        int numbers[] = {-2, 3, 4, 1, 2, 1, 5, -3};
+        // for negative max sum
+        int numberss[] = {-2, -5, -1, -4};
+
         maxsubarray(numbers);
+        maxSubArray(numberss);
 
     }
 }
