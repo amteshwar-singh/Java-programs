8270b59cac4911e9434a411c3ee38e6cbdb65102
Amteshwar Singh <amteshwars8@gmail.com>
2026-09-26 21:06:40 +0530

    h

---
+++ 08arrays/$13stocks.java
@@ -0,0 +1,24 @@
+// buy and sell stocks
+
+public class $13stocks {
+
+    public static int stoocks(int prices[]){ 
+    int buyPrice=Integer.MAX_VALUE;
+    int maxProfit =0;
+    for(int i=0;i<prices.length;i++){
+        if(buyPrice<prices[i]){ //profit
+            int profit=prices[i]-buyPrice;//todays profit
+            maxProfit=Math.max(maxProfit,profit);
+        }
+        else{
+        buyPrice=prices[i];
+        }
+    }
+    return maxProfit;
+}
+public static void main(String args[]) {
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        
+        System.out.println(stoocks(prices));
+    }
+}
